7b648c82a36db2ad6d57f7b38ed4f7885072222e
a7raj <nxaahraj@gmail.com>
2017-12-23 18:54:12 +0600

    N Bringup

---
+++ P6-vendor.mk
@@ -0,0 +1,12 @@
+
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/P6/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/P6/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/P6/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/P6/proprietary/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Symphony/P6/proprietary/xbin,system/xbin)
+
